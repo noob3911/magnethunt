98aa47de256d6043d4fcb2d75c3c17619fb4c576
Satish <satish.p@ensologic.com>
2023-08-20 22:32:26 +0530

    some changes

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
-const torrent1337 = require('./services/1337x')
-const bitSearch = require('./services/bitSearch')
 const Routes = require('./routes/route')
 
 const port = process.env.PORT || 6000;
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use('/', Routes)
