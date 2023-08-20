e7443311145e0b0ddcf6dd0b78e63e3066493146
Satish <satish.p@ensologic.com>
2023-08-20 21:34:49 +0530

    more refined search results

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(express.json());
 
 app.use('/', Routes)
 
+app.get('/',(req,res)=>{
+   res.send("Radhe Radhe !!")
+})
+
 
 app.listen(port, () => {
    console.log(`Server is running on ${port}`);
