3a6047090a8e68f137c00df904f00fe27e7c9623
Satish <satish.p@ensologic.com>
2023-08-20 21:40:05 +0530

    some browser fixes in bitsearch

--- services/bitSearch.js
+++ services/bitSearch.js
@@ -9,7 +9,7 @@ async function bitSearch(query) {
       html = await axios.get(
          url,
          (headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.170 Safari/537.36",
          })
       );
    } catch {
