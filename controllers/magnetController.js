d237da12a760e185b0b65590330892e6652e4aed
Satish <satish.p@ensologic.com>
2023-08-23 01:07:45 +0530

    added new torrents

--- controllers/magnetController.js
+++ controllers/magnetController.js
@@ -1,6 +1,8 @@
 const Controller = require("./controller");
 const torren1337 = require("../services/1337x");
 const bitSearch = require("../services/bitSearch");
+const torLock = require('../services/torlock')
+const torProject = require('../services/torproject')
 
 class MagnetController extends Controller {
    constructor(response) {
@@ -22,7 +24,7 @@ class MagnetController extends Controller {
          //  };
          //  this.sendResponse(combinedResults);
 
-         const [torren1337Results, bitSearchResults] = await Promise.all([
+         const [torren1337Results, bitSearchResults, torResults, torProjectResults] = await Promise.all([
             torren1337(search).catch((error) => {
                console.error("Error from torren1337:", error);
                return [];
@@ -31,15 +33,38 @@ class MagnetController extends Controller {
                console.error("Error from bitSearch:", error);
                return [];
             }),
+            torLock(search).catch((error) => {
+               console.error("Error from bitSearch:", error);
+               return [];
+            }),
+            torProject(search).catch((error) => {
+               console.error("Error from bitSearch:", error);
+               return [];
+            }),
          ]);
          const validTorren1337Results = torren1337Results?.filter((result) => {
             return result.Magnet && result.Magnet.startsWith("magnet:");
          });
 
-         const combinedResults = [...(validTorren1337Results || []), ...(bitSearchResults || [])];
-         combinedResults.sort((a, b) => b.seeders - a.seeders);
-         const top20Results = combinedResults.slice(0, 20);
-         this.sendResponse(top20Results);
+
+         // const combinedResults = [...(validTorren1337Results || []), ...(torResults || [])];
+         // const combo =[...combinedResults, ...(bitSearchResults || []), ...(torProjectResults||[])]
+         // combo.sort((a, b) => b.seeders - a.seeders);
+         // const top20Results = combo.slice(0, 25);
+
+         const combinedResults = [
+            ...(validTorren1337Results || []),
+            ...(torResults || []),
+            ...(bitSearchResults || []),
+            ...(torProjectResults || []),
+          ];
+          
+          combinedResults.sort((a, b) => b.seeders - a.seeders);
+          
+          const top25Results = combinedResults.slice(0, 25);
+
+         this.sendResponse(top25Results);
+
       } catch (error) {
          console.error(error);
       }
